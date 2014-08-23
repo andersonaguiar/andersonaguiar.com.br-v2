ac1f8d255ebf9fde2e014440cf3a5d92963ede44
Anderson Aguiar <revsoft10@gmail.com>
2014-08-23 20:43:27 -0300

    fix blog and add content

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,118 +1,182 @@
 'use strict';
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'assets/js/*.js',
-        'assets/js/plugins/*.js',
-        '!assets/js/scripts.min.js'
-      ]
-    },
-    recess: {
-      dist: {
-        options: {
-          compile: true,
-          compress: true
+    grunt.initConfig({
+        //exec commands
+        exec: {
+            jekyllServe: 'jekyll serve',
+            jekyllBuild: 'jekyll build',
+            npmReset: 'rm -rf node_modules && sudo npm install'
         },
-        files: {
-          'assets/css/main.min.css': [
-            'assets/less/main.less'
-          ]
-        }
-      },
-      dev: {
-        options: {
-          compile: true,
-          compress: false
-        },
-        files: {
-          'assets/css/main.css': [
-            'assets/less/main.less'
-          ]
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          'assets/js/scripts.min.js': [
-            'assets/js/plugins/*.js',
-            'assets/js/_*.js'
-          ]
-        }
-      }
-    },
-    imagemin: {
-      dist: {
-        options: {
-          optimizationLevel: 7,
-          progressive: true
-        },
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: 'images/'
-        }]
-      }
-    },
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.svg',
-          dest: 'images/'
-        }]
-      }
-    },
-    watch: {
-      less: {
-        files: [
-          'assets/less/*.less',
-          'assets/less/bootstrap/*.less'
-        ],
-        tasks: ['recess']
-      },
-      js: {
-        files: [
-          '<%= jshint.all %>'
-        ],
-        tasks: ['jshint','uglify']
-      }
-    },
-    clean: {
-      dist: [
-        'assets/css/main.min.css',
-        'assets/js/scripts.min.js'
-      ]
-    }
-  });
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-recess');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-svgmin');
+        // modify URL
+        replace: {
+            dev: {
+                src: ['_config.yml'],
+                overwrite: true,                 // overwrite matched source files
+                replacements: [{
+                    from: /(url:\s+)''/g,
+                    to: "$1'http://0.0.0.0:4000'"
+                }]
+            },
+            dist: {
+                src: ['_config.yml'],
+                overwrite: true,                 // overwrite matched source files
+                replacements: [{
+                    from: /(url:\s+)''/g,
+                    to: "$1'http://andersonaguiar.github.io/andersonaguiar.com.br-v2'"
+                }]
+            }
+        },
 
-  // Register tasks
-  grunt.registerTask('default', [
-    'clean',
-    'recess',
-    'uglify',
-    'imagemin',
-    'svgmin'
-  ]);
-  grunt.registerTask('dev', [
-    'watch'
-  ]);
+        recess: {
+            dist: {
+                options: {
+                    compile: true,
+                    compress: true
+                },
+                files: {
+                    'assets/css/main.min.css': ['assets/less/main.less']
+                }
+            },
+            dev: {
+                options: {
+                    compile: true,
+                    compress: false
+                },
+                files: {
+                    'assets/css/main.css': ['assets/less/main.less']
+                }
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: {
+                    'assets/js/scripts.min.js': ['assets/js/plugins/*.js', 'assets/js/_*.js']
+                }
+            }
+        },
+
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: '{,*/}*.{png,jpg,jpeg}',
+                    dest: 'images/'
+                }]
+            }
+        },
+
+        svgmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: '{,*/}*.svg',
+                    dest: 'images/'
+                }]
+            }
+        },
+
+        watch: {
+            less: {
+                files: [
+                    'assets/less/*.less',
+                    'assets/less/bootstrap/*.less'
+                ],
+                tasks: ['recess']
+            },
+
+            js: {
+                files: [
+                    'assets/js/**/*.js'
+                ],
+                tasks: ['uglify']
+            },
+
+            jekyll: {
+                files: [
+                    '_layouts/*.html',
+                    '_posts/*.md',
+                    '_includes/*.html',
+                    'assets/**/*',
+                    '_config.yml',
+                    '*.md',
+                    '*.html' 
+                ],
+                tasks: ['exec:jekyllBuild']
+            }
+        },
+
+        'gh-pages': {
+            options: {
+                base: '_site',
+                branch: 'gh-pages',
+                message: 'Publish new post',
+                dotfiles: true
+            },
+            // These files will get pushed to the `bar` branch.
+            src: ['**/*']
+        },
+
+        clean: {
+            dist: [
+                'assets/css/main.min.css',
+                'assets/js/scripts.min.js'
+            ]
+        },
+
+        // Keep multiple browsers & devices in sync when building websites.
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        '_site/**/*'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    // server: {
+                    //     baseDir: "./"
+                    // },
+                    // proxy: {
+                    //     host: "192.168.25.5",
+                    //     port: 4000
+                    // }
+                }
+            }
+        },
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-recess');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-svgmin');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-text-replace');
+
+    // Register tasks
+    grunt.registerTask('default', [
+        'clean',
+        'recess',
+        'uglify',
+        'imagemin',
+        'svgmin'
+    ]);
+
+    grunt.registerTask('dev', ['replace:dev', 'browserSync', 'watch']);
+
+    grunt.registerTask('build', ['replace:dist', 'default', 'exec:jekyllBuild', 'gh-pages']);
 
 };
---
+++ _posts/_2014-07-22-instalando-o-grunt.md
@@ -0,0 +1,25 @@
+Hoje para quem trabalha com front-end é imprescindível o uso de ferramentas de automatização de tarefas, nosso cotidiano é repleto de tarefas que imploram para serem automatizadas(minify de css e js, compilar pré-processadores, executar testes, fazer copy de arquivos e diretórios, otimizar imagens, fazer deploy, etc). 
+
+Atualmente existem várias ferramentas que fazem esse tipo de tarefa para a gente tais como: [Ant](http://http://ant.apache.org/) para JAVA, [Rake](https://rubygems.org/gems/rake) para Ruby e [Fabric](http://www.fabfile.org/) para Python. 
+
+Mas e nós Front-enders, quem poderá nos ajudar?
+
+![chapolin](http://upload.wikimedia.org/wikipedia/pt/4/43/El_Chapolin_Colorado.jpg)
+
+
+## O que é o Grunt?
+É uma ferramenta escrita em JavaScript onde sua finalidade é automatizar tarefas. 
+## Instalação
+
+## Plugins
+
+### #1 Browser Sync
+### #2 Exec
+### #3 Watch
+
+## Na prática
+
+## Exemplo
+
+Um exemplo de projeto utilizando Grunt pode ser encontrado neste [link](https://github.com/andersonaguiar/TDC2014-Grunt), esse foi o mesmo código apresentado na palestra de Grunt no TDC 2014 de Florianópolis, cujo o título foi: [Automatizando tarefas com grunt - O que era chato não é mais](http://www.slideshare.net/andersonagr/automatizando-tarefas-com-grunt)
+
